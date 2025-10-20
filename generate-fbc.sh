2d10019ed78d99e918dddb8b2716141b3c4d72e9
Kevin Hannon <kehannon@redhat.com>
2025-10-20 09:22:43 -0400

    add OCP 4.20 for RHBoK 1.1

--- generate-fbc.sh
+++ generate-fbc.sh
@@ -4,6 +4,6 @@ for OCP_VERSION in v4.16; do
     opm alpha render-template semver $OCP_VERSION/catalog-template.yaml > $OCP_VERSION/catalog/kueue-operator/catalog.json;
 done
 
-for OCP_VERSION in v4.17 v4.18 v4.19; do
+for OCP_VERSION in v4.17 v4.18 v4.19 v4.20; do
     opm alpha render-template semver $OCP_VERSION/catalog-template.yaml --migrate-level=bundle-object-to-csv-metadata > $OCP_VERSION/catalog/kueue-operator/catalog.json;
 done
